fb30265bb1df7473884392f85eecf68be4185756
Koz Ross <koz.ross@retro-freedom.nz>
2024-08-01 08:47:16 +1200

    Xor conformance

--- src/Xor.hs
+++ src/Xor.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE TypeApplications #-}
 
-module Or (cases) where
+module Xor (cases) where
 
 import Data.ByteString (ByteString)
 import PlutusCore
@@ -18,25 +18,25 @@ import PlutusCore.MkPlc (builtin, mkConstant, mkIterAppNoAnn)
 cases :: [Term TyName Name DefaultUni DefaultFun ()]
 cases =
   fmap
-    (mkIterAppNoAnn (builtin () PLC.OrByteString))
-    [ -- orByteString False [] [0xFF] => []
+    (mkIterAppNoAnn (builtin () PLC.XorByteString))
+    [ -- xorByteString False [] [0xFF] => []
       [mkConstant @Bool () False, mkConstant @ByteString () [], mkConstant @ByteString () [0xFF]],
-      -- orByteString False [0xFF] [] => []
+      -- xorByteString False [0xFF] [] => []
       [mkConstant @Bool () False, mkConstant @ByteString () [0xFF], mkConstant @ByteString () []],
-      -- orByteString False [0xFF] [0x00] => [0xFF]
+      -- xorByteString False [0xFF] [0x00] => [0xFF]
       [mkConstant @Bool () False, mkConstant @ByteString () [0xFF], mkConstant @ByteString () [0x00]],
-      -- orByteString False [0x00] [0xFF] => [0xFF]
+      -- xorByteString False [0x00] [0xFF] => [0xFF]
       [mkConstant @Bool () False, mkConstant @ByteString () [0x00], mkConstant @ByteString () [0xFF]],
-      -- orByteString False [0x4F, 0x00] [0xF4] => [0xFF]
+      -- xorByteString False [0x4F, 0x00] [0xF4] => [0xBB]
       [mkConstant @Bool () False, mkConstant @ByteString () [0x4F, 0x00], mkConstant @ByteString () [0xF4]],
-      -- orByteString True [] [0xFF] => [0xFF]
+      -- xorByteString True [] [0xFF] => [0xFF]
       [mkConstant @Bool () True, mkConstant @ByteString () [], mkConstant @ByteString () [0xFF]],
-      -- orByteString True [0xFF] [] => [0xFF]
+      -- xorByteString True [0xFF] [] => [0xFF]
       [mkConstant @Bool () True, mkConstant @ByteString () [0xFF], mkConstant @ByteString () []],
-      -- orByteString True [0xFF] [0x00] => [0xFF]
+      -- xorByteString True [0xFF] [0x00] => [0xFF]
       [mkConstant @Bool () True, mkConstant @ByteString () [0xFF], mkConstant @ByteString () [0x00]],
-      -- orByteString True [0x00] [0xFF] => [0xFF]
+      -- xorByteString True [0x00] [0xFF] => [0xFF]
       [mkConstant @Bool () True, mkConstant @ByteString () [0x00], mkConstant @ByteString () [0xFF]],
-      -- orByteString True [0x4F, 0x00] [0xF4] => [0xFF, 0x00]
+      -- xorByteString True [0x4F, 0x00] [0xF4] => [0xBB, 0x00]
       [mkConstant @Bool () True, mkConstant @ByteString () [0x4F, 0x00], mkConstant @ByteString () [0xF4]]
     ]
